ba89ef173c3e14277c9a56d213bd7395c4fbcaf3
jrynik <jrynik@yahoo.co.uk>
2012-01-03 19:44:58 +0000

    langues (paquet-svp)

---
+++ lang/paquet-svp_sk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svp_description' => 'Ce plugin fournit, d\'une part,  une API permettant de collecter, d\'effectuer des recherches multi-critères et de présenter les informations
+	sur les plugins SPIP - modules fonctionnels, thèmes et squelettes, et d\'autre part, une nouvelle interface d\'administration des plugins gérant les dépendances entre plugins.', # NEW
+	'svp_slogan' => 'Server na zisťovanie informácií o zásuvných moduloch a ich sťahovanie'
+);
+
+?>
