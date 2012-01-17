2381baeb9175d21102042468f47790a5511729b6
kent1 <kent1@arscenic.info>
2012-01-17 07:28:42 +0000

    langues (paquet-svp)

---
+++ lang/paquet-svp_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svp_description' => 'One one hand, this plugin provides an API to perform multi-criteria searches, to collect and present information on SPIP plugins (functional modules, themes, and skeletons) and on the other hand, a new administration interface to manage the dependencies between plugins.',
+	'svp_slogan' => 'Information and download server of plugins'
+);
+
+?>
