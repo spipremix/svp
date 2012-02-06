5ed27b6789ddab7cf00a3531b38f3c6a3e150202
eric <eric@smellup.net>
2012-02-06 11:34:43 +0000

    langues (paquet-svp)

--- lang/paquet-svp_en.php
+++ lang/paquet-svp_en.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// S
-	'svp_description' => 'One one hand, this plugin provides an API to perform multi-criteria searches, to collect and present information on SPIP plugins (functional modules, themes, and skeletons) and on the other hand, a new administration interface to manage the dependencies between plugins.',
+	'svp_description' => 'On one hand, this plugin provides an API to perform multi-criteria searches, to collect and present information on SPIP plugins (functional modules, themes, and skeletons) and on the other hand, proposes a new administration interface to manage the dependencies between plugins.',
 	'svp_slogan' => 'Information and download server of plugins'
 );
 
