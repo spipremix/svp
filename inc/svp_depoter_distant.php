90d1c5fdd546f17ce5c8e25a107e4668deb48616
eric <eric@smellup.net>
2012-02-29 17:28:39 +0000

    Correction des nom et slogan d'un plugin à paquets multiples. Fonctionne pour une première insertion : mise à jour à vérifier.

--- inc/svp_depoter_distant.php
+++ inc/svp_depoter_distant.php
@@ -414,20 +414,17 @@ function svp_actualiser_paquets($id_depot, $paquets, &$nb_paquets, &$nb_plugins,
 					if (!$plugin AND !array_key_exists($insert_plugin['prefixe'], $insert_plugins)) {
 						$insert_plugins[ $insert_plugin['prefixe'] ] = array_merge($insert_plugin, array('vmax' => $insert_paquet['version']));
 					}
-                    elseif (array_key_exists($insert_plugin['prefixe'], $insert_plugins)
-                        AND (spip_version_compare($insert_plugins[ $insert_plugin['prefixe'] ]['vmax'], $insert_paquet['version'], '<=')))
-                    { // attribuer au plugin le nom et le slogan du paquet le plus à jour
-                        $insert_plugins[ $insert_plugin['prefixe'] ] = array_merge($insert_plugin,
-                            array(
-                                'nom' => $insert_plugin['nom'],
-                                'slogan' => $insert_plugin['slogan']
-                            ));
-                    }
 					else {
-
 						if ($plugin) {
 							$id_plugin = $plugin['id_plugin'];
 							$prefixes[$insert_plugin['prefixe']] = $id_plugin;
+						}
+						if (array_key_exists($insert_plugin['prefixe'], $insert_plugins)
+						AND (spip_version_compare($insert_plugins[ $insert_plugin['prefixe'] ]['vmax'], $insert_paquet['version'], '<='))) {
+							// attribuer au plugin le nom et le slogan du paquet le plus à jour
+							$insert_plugins[ $insert_plugin['prefixe'] ]['nom'] = $insert_plugin['nom'];
+							$insert_plugins[ $insert_plugin['prefixe'] ]['slogan'] = $insert_plugin['slogan'];
+							$insert_plugins[ $insert_plugin['prefixe'] ]['vmax'] = $insert_paquet['version'];
 						}
 					}
 	
