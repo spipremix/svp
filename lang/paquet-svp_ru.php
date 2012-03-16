b678f37cb92790887cf54b27ffe168002e7406f4
n.komar <n.komar@aloha.org.ua>
2012-03-16 15:17:45 +0000

    langues (paquet-svp)

---
+++ lang/paquet-svp_ru.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-svp?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svp_description' => 'Дополнение предоставляет API для выполнения различных критериев поиска, сбора и представления информации о SPIP плагинов (функциональные модули, темы и скелеты), а с другой стороны, предлагает новый интерфейс администрирования для управления зависимостями между плагинами .',
+	'svp_slogan' => 'Информация и сервер загрузки плагина.'
+);
+
+?>
