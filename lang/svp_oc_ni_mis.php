eb90e805c76794b28b854bda790511f010d4160a
sylvain <sylvain@oliver-fr.org>
2015-03-03 04:34:18 +0000

    [Salvatore] [source:_core_/plugins/svp/lang/ svp] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/svp_oc_ni_mis.php
@@ -0,0 +1,171 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/svp?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'actions_a_faire' => 'Acioun a tratà',
+	'actions_demandees' => 'Acioun demandadi :',
+	'actions_non_traitees' => 'Acioun noun realisadi',
+	'actions_realises' => 'Acioun realisadi',
+	'afficher_les_plugins_incompatibles' => 'Afichà lu plugin incoumpatible',
+	'alerte_compatibilite' => 'Coumpatibilità fourçada',
+
+	// B
+	'bouton_activer' => 'Ativà',
+	'bouton_actualiser' => 'Atualisà',
+	'bouton_actualiser_tout' => 'Atualisà lu depòsiti',
+	'bouton_appliquer' => 'Aplicà',
+	'bouton_confirmer' => 'Counfirmà',
+	'bouton_desactiver' => 'Desativà',
+	'bouton_desinstaller' => 'Desinstalà',
+	'bouton_installer' => 'Telecargà e ativà',
+	'bouton_modifier_depot' => 'Moudificà lou depòsiti',
+	'bouton_supprimer' => 'Suprimà',
+	'bouton_up' => 'Metre a jou',
+	'bulle_actualiser_depot' => 'Atualisà lu paquet dóu depòsiti',
+	'bulle_actualiser_tout_depot' => 'Atualisà lu paquet de toui lu depòsiti',
+	'bulle_afficher_xml_plugin' => 'Countengut dóu fichié XML dau plugin',
+	'bulle_ajouter_spipzone' => 'Ajustà lou depòsiti SPIP-Zone',
+	'bulle_aller_depot' => 'Anà sus la pàgina d’aqueu depòsiti',
+	'bulle_aller_documentation' => 'Anà sus la pàgina de doucumentacioun',
+	'bulle_aller_plugin' => 'Anà sus la pàgina dóu plugin',
+	'bulle_supprimer_depot' => 'Suprimà lou depòsiti e lu siéu paquet',
+	'bulle_telecharger_archive' => 'Telecargà l’arquivi',
+	'bulle_telecharger_fichier_depot' => 'Telecargà lou fichié XML dóu depòsiti',
+	'bulle_telecharger_librairie' => 'Telecargà la biblioutéca',
+
+	// C
+	'cacher_les_plugins_incompatibles' => 'Escoundre lu plugin incoumpatible',
+	'categorie_aucune' => 'Sensa categourìa',
+	'categorie_outil' => 'Óutis de desfouloupamen',
+	'categorie_squelette' => 'Esquèletrou',
+	'config_depot_editable' => 'Permetre l’edicioun dei depòsiti ?',
+
+	// F
+	'fieldset_debug' => 'Debug',
+	'fieldset_edition' => 'Edicioun',
+	'fieldset_fonctionnement' => 'Founciounamen',
+
+	// I
+	'info_1_depot' => '1 depòsiti',
+	'info_1_paquet' => '1 paquet',
+	'info_1_plugin' => '1 plugin',
+	'info_admin_plugin_verrou_tous' => 'Aquela pàgina lista toui lu plugin dóu sit.',
+	'info_adresse_spipzone' => 'SPIP-Zone - Plugin',
+	'info_aucun_depot' => 'mìnga depòsiti',
+	'info_aucun_paquet' => 'mìnga paquet',
+	'info_aucun_plugin' => 'mìnga plugin',
+	'info_compatibilite_dependance' => 'Per @compatibilite@ :',
+	'info_depots_disponibles' => '@total_depots@ depòsiti',
+	'info_nb_depots' => '@nb@ depòsiti',
+	'info_nb_paquets' => '@nb@ paquet',
+	'info_nb_plugins' => '@nb@ plugin',
+	'info_paquets_disponibles' => '@total_paquets@ paquet dispounible',
+	'info_plugin_incompatible' => 'versioun incoumpatible',
+	'info_plugin_installe' => 'ja instalat',
+	'info_plugin_obsolete' => 'versioun perimada',
+	'info_plugins_disponibles' => '@total_plugins@ plugin dispounible',
+	'info_type_depot_git' => 'Depòsiti gerat souta GIT',
+	'info_type_depot_manuel' => 'Depòsiti gerat manualamen',
+	'info_type_depot_svn' => 'Depòsiti gerat souta SVN',
+	'info_verrouille' => 'Impoussible de desativà o de desinstalà aqueu plugin.',
+
+	// L
+	'label_1_autre_contribution' => 'autra countributioun',
+	'label_actualise_le' => 'Atualisat lou',
+	'label_archive' => 'URL da l’arquivi',
+	'label_branches_spip' => 'Coumpatible',
+	'label_categorie' => 'Categourìa',
+	'label_compatibilite_spip' => 'Coumpatibilità',
+	'label_critere_categorie' => 'Categourìa',
+	'label_critere_depot' => 'Depòsiti',
+	'label_critere_doublon' => 'Coumpatibilità',
+	'label_critere_phrase' => 'Recercà en lu plugin',
+	'label_modifie_le' => 'Moudificat lou',
+	'label_prefixe' => 'Prefis',
+	'label_selectionner_plugin' => 'Seleciounà aqueu plugin',
+	'label_tags' => 'Tag',
+	'label_type_depot_git' => 'Depòsiti souta GIT',
+	'label_type_depot_manuel' => 'Depòsiti manual',
+	'label_type_depot_svn' => 'Depòsiti souta SVN',
+	'label_url_archives' => 'URL dóu countengut dei arquivi',
+	'label_url_brouteur' => 'URL de la raïs dei sourgent',
+	'label_url_serveur' => 'URL dóu servidour',
+	'label_version' => 'Versioun',
+	'label_xml_depot' => 'Fichié XML dóu depòsiti',
+	'label_xml_plugin' => 'XML',
+	'legende_installer_plugins' => 'Instalà dei plugin',
+	'legende_rechercher_plugins' => 'Recercà dei plugin',
+
+	// M
+	'message_action_finale_getlib_fail' => 'L’instalacioun de la biblioutéca « @plugin@ » a souhit',
+	'message_action_finale_getlib_ok' => 'La biblioutéca « @plugin@ » es estada instalada',
+	'message_action_finale_install_fail' => 'L’instalacioun dóu plugin « @plugin@ » (versioun : @version@) a souhit',
+	'message_action_get' => 'Telecargà lou plugin « @plugin@ » (versioun : @version@)',
+	'message_action_getlib' => 'Telecargà la biblioutéca « <a href="@version@" class="spip_out">@plugin@</a> »',
+	'message_action_geton' => 'Telecargà a ativà lou plugin « @plugin@ » (versioun : @version@)',
+	'message_action_kill' => 'Supressioun dei fichié dóu plugin « @plugin@ » (versioun : @version@)',
+	'message_action_off' => 'Desativà lou plugin « @plugin@ » versioun : @version@)',
+	'message_action_on' => 'Ativà lou plugin « @plugin@ » (versioun : @version@)',
+	'message_action_stop' => 'Desinstalà lou plugin « @plugin@ » (versioun : @version@)',
+	'message_action_up' => 'Metuda a jou dóu plugin « @plugin@ » (da la versioun @version@ a @version_maj@)',
+	'message_action_upon' => 'Metuda a jou e ativacioun dóu plugin « @plugin@ » (versioun : @version@)',
+	'message_erreur_aucun_plugin_selectionne' => 'Mìnga plugin seleciounat.',
+	'message_erreur_maj_inconnu' => 'Metuda a jou impoussibla d’un plugin escounouissut.',
+	'message_erreur_plugin_non_actif' => 'Es empoussible de desativà un plugin noun atiéu.',
+	'message_nok_champ_obligatoire' => 'Aqueu camp es óubligatori',
+	'message_nok_url_archive' => 'L’URL de l’arquivi es invalide',
+	'message_nok_url_depot_incorrecte' => 'L’adressa « @url@ » es mau couret',
+	'message_ok_depot_ajoute' => 'Lou depòsiti « @url@ » es estat ajustat.',
+
+	// O
+	'onglet_depots' => 'Gerà lu depòsiti',
+	'option_categorie_toute' => 'Touti li categourìa',
+	'option_depot_tout' => 'Toui lu depòsiti',
+	'option_doublon_non' => 'La darrièra versioun',
+	'option_doublon_oui' => 'Touti li versioun coumpatibli',
+
+	// P
+	'placeholder_phrase' => 'prefis, noum, eslogan, descritiéu o autour',
+	'plugin_info_actif' => 'Plugin atiéu',
+	'plugin_info_up' => 'Una metuda a jou dóu plugin es dispounible (versioun @version@)',
+	'plugin_info_verrouille' => 'Plugin ferouiat',
+	'plugins_inactifs_liste' => 'Inatiéu',
+	'plugins_non_verrouilles_liste' => 'Noun ferouiat',
+	'plugins_verrouilles_liste' => 'Ferouiat',
+
+	// R
+	'resume_table_depots' => 'Lista dei depòsiti ajustat',
+	'resume_table_paquets' => 'Lista dei paquet',
+	'resume_table_plugins' => 'Lista dei plugin @categorie@',
+
+	// T
+	'titre_depot' => 'Depòsiti',
+	'titre_depots' => 'Depòsiti',
+	'titre_form_ajouter_depot' => 'Ajustà un depòsiti',
+	'titre_form_charger_plugin' => 'Recercà e ajustà dei plugin',
+	'titre_form_charger_plugin_archive' => 'Telecargà un plugin da la siéu arquivi',
+	'titre_form_configurer_svp' => 'Counfigurà lou Servidour de Plugins',
+	'titre_liste_autres_depots' => 'Autre depòsiti',
+	'titre_liste_depots' => 'Lista dei depòsiti dispounible',
+	'titre_liste_paquets_plugin' => 'Lista dei paquet dóu plugin',
+	'titre_liste_plugins' => 'Lista dei plugin',
+	'titre_logo_depot' => 'Logou dóu depòsiti',
+	'titre_logo_plugin' => 'Logou dóu plugin',
+	'titre_nouveau_depot' => 'Depòsiti nouvèu',
+	'titre_page_configurer' => 'Servidour de Plugin',
+	'titre_paquet' => 'Paquet',
+	'titre_paquets' => 'Paquet',
+	'titre_plugin' => 'Plugin',
+	'titre_plugins' => 'Plugin',
+	'tout_cocher' => 'Entacà tout',
+	'tout_cocher_up' => 'Entacà li metudi a jou',
+	'tout_decocher' => 'Destacà tout'
+);
+
+?>
