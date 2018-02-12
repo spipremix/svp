37dc3946d686f8ab53d8c9b4d59fd669d483057a
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-12 04:36:13 +0000

    [Salvatore] [source:_core_/plugins/svp/lang/ paquet-svp] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-svp_br.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-svp?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svp_slogan' => 'SerVijer titouriñ hag enrollañ ar Plugins'
+);
