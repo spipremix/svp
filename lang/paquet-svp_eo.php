9e0d6d355c2aee7648f4af402aa890c9b22c3bbc
gpl <gpl@long-player.net>
2018-03-08 04:36:45 +0000

    [Salvatore] [source:_core_/plugins/svp/lang/ paquet-svp] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-svp_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-svp?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'svp_description' => 'Tiu kromprogramo disponigas, unue, API-on ebligante plurkriterie serĉi, kolekti kaj prezenti informojn pri SPIP-kromprogramoj (funkciaj, aspektaj kaj skeletaj), kaj, due, nova interfacon de mastrumado de kromprogramoj, kiu zorgas pri la dependeco inter la kromprogramoj.',
+	'svp_slogan' => 'SerVilo pri Informo kaj elŝuto de kromProgramoj'
+);
